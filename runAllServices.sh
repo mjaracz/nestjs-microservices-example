8602b4b04b9eb4a3124b7d12e8549dfda0c66726
michal19996@gmail.com <michal19996@gmail.com>
2022-08-11 09:24:50 +0200

    refactor, delete old .sh file

--- runAllServices.sh
+++
@@ -1,6 +0,0 @@
-# install dependencies for each service
-for dir in ./*/; do sh -c "cd ${dir} && yarn install"; done
-# start mongoDB and NATS service queue
-docker-compose up -d
-# launch all service
-for dir in ./*/; do sh -c "cd ${dir} && yarn start &"; done
